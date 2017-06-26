12987fc9485f587f77654d310923c7c25bff40f3
mallikarjun.321 <mallikarjun.321@gmail.com>
2017-06-26 20:08:28 +0100

    v3

--- Valtech/src/Test2.java
+++ Valtech/src/Test2.java
@@ -21,7 +21,9 @@ public class Test2 {
 		String a[] = { "ABOUT", "WORK", "SERVICES" };
 		for (int i = 1; i < 4; i++) {
 			driver.findElement(By.xpath(".//*[@id='navigationMenuWrapper']/div/ul/li[" + i + "]/a/span")).click();
-			Assert.assertTrue(driver.findElement(By.tagName("h1")).getText().equalsIgnoreCase(a[i - 1]));
+			System.out.println(driver.findElement(By.xpath(".//*[@id='container']//following::h1")).getText());
+			
+			Assert.assertTrue(driver.findElement(By.xpath(".//*[@id='container']//following::h1")).getText().equalsIgnoreCase(a[i - 1]));
 
 		}
 		driver.close();
